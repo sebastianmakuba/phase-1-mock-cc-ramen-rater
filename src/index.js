6d0777502e2ac45d1e1193e0d589508bb219543a
+ <+>
2023-06-24 19:58:05 +0300

    displaying

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
 // write your code here
+document.addEventListener("DOMContentLoaded", () => {
+    fetchRamen()
+})
+function fetchRamen() {
+    fetch(" http://localhost:3000/ramens")
+    .then (response  => response.json())
+    .then(data => displayRamen(data))
+}
+function displayRamen (data) {
+    data.forEach(ramen => {
+       const parentDiv = document.getElementById("ramen-menu") 
+       const menuDetails = document.createElement ('div')
+       menuDetails.innerHTML = `
+       <div class = photo> 
+       <img src = ${ramen.image}>
+       </div>`
+       menuDetails.addEventListener('click',() => {
+        ramenDetails(ramen)})
+
+       parentDiv.appendChild(menuDetails)
+      
+    });
+   
+    }
+    // //when clicked
+    function ramenDetails(ramen){
+        // const ramenDetail = document.querySelector('#ramen-detail')
+        const detailImage = document.querySelector('#ramen-detail .detail-image')
+        const name = document.querySelector('#ramen-detail .name')
+        const restaurant = document.querySelector('#ramen-detail .restaurant')
+        const ratingDisplay = document.querySelector('#rating-display')
+        const commentDisplay = document.querySelector('#comment-display')
+
+        detailImage.src = ramen.image
+        detailImage.alt = ramen.name
+        restaurant.textContent = ramen.restaurant
+        name.textContent = ramen.name
+        ratingDisplay.textContent = ramen.rating
+        commentDisplay.textContent = ramen.comment
+
+    }
+       
+        
+
+
+        
+    
+             
+
+    
+
